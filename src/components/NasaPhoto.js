b050605f70161e6eb0f7b66e888f0d07350bbdda
Ryan Howard <ryanghoward@protonmail.com>
2021-11-03 13:54:09 -0700

    Home nav built and added to NasaPhoto.js

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import NavBar from './NavBar';
 
 const apiKey = process.env.REACT_APP_NASA_KEY;
 
@@ -22,29 +23,32 @@ function NasaPhoto() {
     if (!photoData) return <div />;
 
     return (
-        <div>
-            {photoData.media_type === 'image' ? (
-                <img 
-                    src={photoData.url}
-                    alt={photoData.title}
-                />
-            ) : (
-                <iframe
-                    title='space-video'
-                    src={photoData.url}
-                    frameBorder='0'
-                    // gesture='media'
-                    allow='encrypted-media'
-                    allowFullScreen
-                    className='photo'
-                    />
-            )}
+        <>
+            <NavBar />
             <div>
-                <h1>{photoData.title}</h1>
-                <p>{photoData.date}</p>
-                <p>{photoData.explanation}</p>
+                {photoData.media_type === 'image' ? (
+                    <img 
+                        src={photoData.url}
+                        alt={photoData.title}
+                    />
+                ) : (
+                    <iframe
+                        title='space-video'
+                        src={photoData.url}
+                        frameBorder='0'
+                        // gesture='media'
+                        allow='encrypted-media'
+                        allowFullScreen
+                        className='photo'
+                        />
+                )}
+                <div>
+                    <h1>{photoData.title}</h1>
+                    <p>{photoData.date}</p>
+                    <p>{photoData.explanation}</p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
