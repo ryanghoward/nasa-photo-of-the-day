e71eaa486e5a9f75cf2995797f6bf640acbfa3b8
Ryan Howard <ryanghoward@protonmail.com>
2021-11-03 12:30:55 -0700

    Home Link route built in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div>
-            
+            <Link to='/nasaphoto'>See into the stars!</Link>
         </div>
     )
 }
