161772258433a5782a1c56a0d65544f82bf58446
Ryan Howard <ryanghoward@protonmail.com>
2021-11-03 12:45:34 -0700

    Nasa data received and image rendering

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 function NasaPhoto() {
+    const [photoData, setPhotoData] = useState(null);
+
+    useEffect(() => {
+        fetchPhoto();
+
+        async function fetchPhoto() {
+            const res = await fetch(
+                `https://api.nasa.gov/planetary/apod?api_key=MkhbUiVgpaT5zbapm2sAtydg9lwQTQsPLvGonkyr`
+            );
+            const data = await res.json();
+            setPhotoData(data);
+        }
+    }, []);
+
+    if (!photoData) return <div />;
+
     return (
         <div>
-            
+            <img 
+                src={photoData.url}
+                alt={photoData.title}
+            />
         </div>
     )
 }
