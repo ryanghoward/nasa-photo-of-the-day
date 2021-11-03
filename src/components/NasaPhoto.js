fb91a29d69abb1c8558b90f75a0460801e9d9c97
Ryan Howard <ryanghoward@protonmail.com>
2021-11-03 13:12:31 -0700

    Img or iframe ternary added to fetchPhoto component

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -21,10 +21,22 @@ function NasaPhoto() {
 
     return (
         <div>
-            <img 
-                src={photoData.url}
-                alt={photoData.title}
-            />
+            {photoData.media_type === 'image' ? (
+                <img 
+                    src={photoData.url}
+                    alt={photoData.title}
+                />
+            ) : (
+                <iframe
+                    title='space-video'
+                    src={photoData.url}
+                    frameBorder='0'
+                    gesture='media'
+                    allow='encrypted-media'
+                    allowFullScreen
+                    className='photo'
+                    />
+            )}
             <div>
                 <h1>{photoData.title}</h1>
                 <p>{photoData.date}</p>
